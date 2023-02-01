7e48909d92eccccf1e75ee88582311b7260be585
basheeromy <basheerkomassery@gmail.com>
2023-02-01 13:45:55 +0530

    this program contains an infinite loop and it uses boolean expression

---
+++ bool.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(void)
+{
+    while(true)
+    {
+        printf("hi\n");
+    }
+}
+
+/* this is an infinite loop. so use clt + c to stop this process 
+in terminal . 
+stdbool.h is the library for boolian expressions */
