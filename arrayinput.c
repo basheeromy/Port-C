314bc0ec1d22ae64a6d2b878759e564fa59c3900
basheeromy <basheerkomassery@gmail.com>
2023-02-01 14:37:07 +0530

    created a simple program to create an array and print elements in the array

---
+++ arrayinput.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int elements = 10;
+    printf("Number of elements to store: ");
+    scanf("%d",&elements);
+
+    printf("Numbers: \n");
+
+    int array[elements];
+
+    for(int i = 0;i < elements;i++)
+    {
+
+        scanf("%d",& array[i]);
+        
+    }
+    
+    for (int i = 0; i < elements; i++)
+    {
+        printf("%i,",array[i]);
+    }
+    printf("\n");
+}
