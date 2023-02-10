4120f86944e38b214aa52f2472cbaf7e6edda0e2
basheeromy <basheerkomassery@gmail.com>
2023-02-10 15:42:16 +0530

    edited progame to avoid unnecessary iteration in second loop, changed continue to break.

--- countdup.c
+++ countdup.c
@@ -30,7 +30,7 @@ int main(void)
             if(array[j] == array[i])
             {
                 exit = true;
-                continue;
+                break;
                 
             }
         }
