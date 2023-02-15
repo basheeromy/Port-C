e2b2cbfff060d469063d86ee3e9bc8be0857ceee
basheeromy <basheerkomassery@gmail.com>
2023-02-16 04:14:38 +0530

    This program contains Function wich taking inputs as arguments

---
+++ function2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// declare a function wich takes arguments and not pass return value.
+void sum(int num1,int num2);
+
+int main(void)
+{
+    int a,b;
+    printf("Input two numbers to see sum : \n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    // call function with arguments passed
+
+    sum(a,b);
+    return 0;
+}
+
+// function definition and recieving arguments.
+void sum(int num1,int num2)
+{
+    int total = num1 + num2;
+    printf("The sum of number given is : %i\n", total);
+}
